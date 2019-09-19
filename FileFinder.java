f17580f79a2fb5f8cced889c1b291a0a3f201d40
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-19 17:04:30 +0530

    added user input

--- FileFinder.java
+++ FileFinder.java
@@ -1,29 +1,46 @@
 package assignmnet1;
 
 import java.io.File;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileFinder {
-	static public void findfilesbyregex(String directory,String regexpattern){
+	static public void findFilesByRegex(File[] files, String regexPattern) {
 		try {
-			File path = new File(directory);
-			File [] files = path.listFiles();
-			for (int i = 0; i < files.length; i++){
-				if (files[i].isFile()){
-					Pattern pattern = Pattern.compile(regexpattern);
-					Matcher matcher = pattern.matcher(files[i].getName()); 
-					 if (matcher.matches()) {
-					      System.out.println(files[i]);
-					 }
+			for (File file: files) {
+				
+				if (file.isFile()) {
+					Pattern pattern = Pattern.compile(regexPattern);
+					Matcher matcher = pattern.matcher(file.getName());
+					if (matcher.matches()) {
+						System.out.println(file);
+					}
+				}		
+				else if (file.isDirectory()) {
+					File[] subFiles = new File(file.getPath()).listFiles();
+					if (subFiles != null) {
+						findFilesByRegex(subFiles, regexPattern);
 					}
 				}
-			}catch (Exception e) {
-				e.printStackTrace();
-				}
+				
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
+
 	public static void main(String[] args) {
-		findfilesbyregex("/home/zadmin/Desktop/mydetails","(.*/)*.+\\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG)$");
+		File[] files = new File("/home/").listFiles();
+		 Scanner scanner = new Scanner(System.in);
+	        while(true)
+	        {
+	            System.out.println("Enter the regular expression");
+	            String regexPattern = scanner.next();
+	            findFilesByRegex(files,regexPattern);
+	        }
 	}
 
 }
+
