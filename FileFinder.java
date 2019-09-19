e4e53183e3e7922dcfeb0cc2c77bfcc70fb4b970
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-19 17:07:05 +0530

    added comments

--- FileFinder.java
+++ FileFinder.java
@@ -6,17 +6,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileFinder {
+	/*given files and regular expression finds all the files matching regular expression*/
 	static public void findFilesByRegex(File[] files, String regexPattern) {
 		try {
 			for (File file: files) {
-				
+				//if it is file match with regular expression
 				if (file.isFile()) {
 					Pattern pattern = Pattern.compile(regexPattern);
 					Matcher matcher = pattern.matcher(file.getName());
 					if (matcher.matches()) {
 						System.out.println(file);
 					}
-				}		
+				}
+				//if it is a directory search inside directory
 				else if (file.isDirectory()) {
 					File[] subFiles = new File(file.getPath()).listFiles();
 					if (subFiles != null) {
@@ -33,7 +35,8 @@ public class FileFinder {
 
 	public static void main(String[] args) {
 		File[] files = new File("/home/").listFiles();
-		 Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		
 	        while(true)
 	        {
 	            System.out.println("Enter the regular expression");
